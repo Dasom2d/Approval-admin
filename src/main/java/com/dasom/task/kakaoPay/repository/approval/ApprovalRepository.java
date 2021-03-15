e86c3c367a91c08c0631d84aaa2ab36c077044e3
이다솜 <dasom2d@gmail.com>
2021-03-15 18:31:44 +0900

    [ADD] 기안 등록, 수정, 삭제, 조회 기능 추가
    - 쿼리 작성
    - respoitory 작성
    - 관련 테스트코드 작성

---
+++ src/main/java/com/dasom/task/kakaoPay/repository/approval/ApprovalRepository.java
@@ -0,0 +1,39 @@
+package com.dasom.task.kakaoPay.repository.approval;
+
+import com.dasom.task.kakaoPay.model.approval.Approval;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class ApprovalRepository {
+
+    @Autowired
+    private SqlSessionTemplate sqlSessionTemplate;
+
+    private static final String PREFIX = ApprovalRepository.class.getCanonicalName() + ".";
+
+
+    public Approval.ApprovalDocument getApproval(Approval.Search search) {
+        return sqlSessionTemplate.selectOne(PREFIX + "getApproval", search);
+    }
+
+    public List<Approval.ApprovalDocument> getApprovalList(Approval.Search search) {
+        return sqlSessionTemplate.selectList(PREFIX + "getApprovalList", search);
+    }
+
+    public Integer registerApproval(Approval.AddParam addParam) {
+        return sqlSessionTemplate.insert(PREFIX + "insertApproval", addParam);
+    }
+
+    public Integer updateApproval(Approval.AddParam addParam) {
+        return sqlSessionTemplate.insert(PREFIX + "updateApproval", addParam);
+    }
+
+    public Integer deleteApproval(Approval.AddParam addParam) {
+        return sqlSessionTemplate.delete(PREFIX + "deleteApproval", addParam);
+    }
+
+}
