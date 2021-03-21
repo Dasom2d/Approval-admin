7db15e35339472685e97a70f4b80ae304956420c
이다솜 <dasom2d@gmail.com>
2021-03-21 18:00:32 +0900

    [ADD] frontend test - Main.vue ; 로그인여부 확인 테스트

--- frontend/tests/unit/main.spec.js
+++ frontend/tests/unit/main.spec.js
@@ -1,4 +1,4 @@
-// import { expect } from 'chai'
+import { expect } from 'chai'
 import { shallowMount, createLocalVue } from '@vue/test-utils';
 import MainPage from '@/components/view/main/Main.vue'
 import Vuex from 'vuex';
@@ -9,26 +9,30 @@ localVue.use(Vuex);
 describe('Main.vue', () => {
   let store;
   let wrapper;
-  let loginMember = {
-    member: {
-      memberId: 1,
-      name: '라이언',
-      gradeId: 1,
-      gradeName: '회장'
+  let temporaryData = {
+    loginMember: {
+      member: {
+        memberId: 1,
+        name: '라이언',
+        gradeId: 1,
+        gradeName: '회장'
+      }
     }
   }
 
   beforeEach(() => {
     store = new Vuex.Store({
-      state: loginMember
+      state: temporaryData
     });
     wrapper = shallowMount(MainPage, { store, localVue });
   });
 
 
-  it('로그인정보가없는가', () => {
+  it('로그인정보가존재하는가', () => {
     console.log('test start')
-    console.log(store.state.member.name);
-    console.log(wrapper.vm);
+    let expectedMemberName = store.state.loginMember.member.name;
+    let resultMemberName = wrapper.vm.$store.state.loginMember.member.name;
+
+    expect(expectedMemberName).to.equal(resultMemberName);
   })
 })
